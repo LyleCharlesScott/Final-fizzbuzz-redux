b92609ad8a719f2691b96d3bc6809433cac6a2d1
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-10-02 12:46:47 -0700

    refactor main.js write function to use sea-d44-fizz-buzz-ls output

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,35 @@
 "use strict";
 
+var FizzBuzz = require ('sea-d44-fizz-buzz-ls');
+
+// var buzzer = new FizzBuzz('pen', 'island');
+// buzzer.input(15, 2);
+
+// console.log(buzzer.output());
+
+write = function(instance, destination) {
+  var fbOutputArray = instance.output();
+  var fbOutputTitle = "Low number: " + "GET THE LOW NUMBER HERE SOMEHOW" + " High number: " + "GET THE HIGH NUMBER HERE SOMEHOW";
+  var fbOutputString = ""; 
+  for (var j=0; j < (fbOutputArray.length); j++) {
+    fbOutputString += (fbOutputArray[j] + " ");
+  };
+  var resultSectionOuterBox = document.createElement("div");
+  resultSectionOuterBox.className = "output l-output";
+  var resultTitleContainer = document.createElement("p");
+  var resultContainer = document.createElement("p");
+  var text1 = document.createTextNode(fbOutputTitle);
+  var text2 = document.createTextNode(fbOutputString);
+  resultTitleContainer.appendChild(text1);
+  resultContainer.appendChild(text2);
+  resultSectionOuterBox.appendChild(resultTitleContainer);
+  resultSectionOuterBox.appendChild(resultContainer);
+  destination.insertBefore(resultSectionOuterBox, destination.firstChild);
+};
+
 // create two Fizzbuzzes just for sport
 var fbFizzBuzz = new Fizzbuzz();
 var fbBleepBlorp = new Fizzbuzz('Bleep', 'Blorp');
-console.log ("Outside of the routine", fbFizzBuzz, fbBleepBlorp);
 
 // activate first FizzBuzz input area
 document.getElementById("submit").addEventListener("click", function(e) {
@@ -11,7 +37,8 @@ document.getElementById("submit").addEventListener("click", function(e) {
   fbFizzBuzz.read(parseInt(document.getElementById('number-one').value),parseInt(document.getElementById('number-two').value));
   document.getElementById('number-one').value = "";
   document.getElementById('number-two').value = "";
-  fbFizzBuzz.write(document.getElementById("result-box"));
+  write (fbfizzbuzz, (document.getElementById("result-box"));
+  // fbFizzBuzz.write(document.getElementById("result-box"));
 });
 
 document.getElementById("clear").addEventListener("click", function(e) {
@@ -25,11 +52,11 @@ document.getElementById("submitb").addEventListener("click", function(e) {
   fbBleepBlorp.read(parseInt(document.getElementById('number-oneb').value), parseInt(document.getElementById('number-twob').value));
   document.getElementById('number-oneb').value = "";
   document.getElementById('number-twob').value = "";
-  fbBleepBlorp.write(document.getElementById("result-boxb"));
+  write (fbBleepBlorp, (document.getElementById("result-boxb"));
+  // fbBleepBlorp.write(document.getElementById("result-boxb"));
 });
 
 document.getElementById("clearb").addEventListener("click", function(e) {
   e.preventDefault();
   fbBleepBlorp.emptyBox(document.getElementById("result-boxb"));
 });
-
