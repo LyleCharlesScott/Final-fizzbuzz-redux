765f658564af9edff15a0750a9daee98f1271518
Lyle Scott <Lyle.C.Scott@gmail.com>
2015-10-05 10:22:40 -0700

    final version

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 module.exports = (function() {
   
@@ -18,7 +18,7 @@ module.exports = (function() {
       if (i%5 === 0) {
         output += secondBuzz; 
       }
-      if (output === "") { 
+      if (output === '') { 
         output = i; 
       }
       fbOutputArray.push(output);
@@ -50,7 +50,7 @@ module.exports = (function() {
   return FizzBuzz;
 })();
 },{}],2:[function(require,module,exports){
-"use strict";
+'use strict';
 
 var FizzBuzz = require ('sea-d44-fizz-buzz-ls');
 
@@ -70,15 +70,14 @@ var fizzBuzzFrontEnd = (function() {
 
   var write = function(instance, destination, firstUserInputNumber, secondUserInputNumber) {
     var fbOutputArray = instance.output();
-    var fbOutputTitle = "First input: " + firstUserInputNumber + " Second input: " + secondUserInputNumber;
-    var fbOutputString = ""; 
+    var fbOutputTitle = 'First input: ' + firstUserInputNumber + ' Second input: ' + secondUserInputNumber;
+    var fbOutputString = ''; 
     for (var j=0; j < (fbOutputArray.length); j++) {
-      fbOutputString += (fbOutputArray[j] + " ");
+      fbOutputString += (fbOutputArray[j] + ' ');
     };
-    var resultSectionOuterBox = document.createElement("div");
-    resultSectionOuterBox.className = "output l-output";
-    var resultTitleContainer = document.createElement("p");
-    var resultContainer = document.createElement("p");
+    var resultSectionOuterBox = document.createElement('div');
+    var resultTitleContainer = document.createElement('p');
+    var resultContainer = document.createElement('p');
     var text1 = document.createTextNode(fbOutputTitle);
     var text2 = document.createTextNode(fbOutputString);
     resultTitleContainer.appendChild(text1);
@@ -86,6 +85,7 @@ var fizzBuzzFrontEnd = (function() {
     resultSectionOuterBox.appendChild(resultTitleContainer);
     resultSectionOuterBox.appendChild(resultContainer);
     destination.insertBefore(resultSectionOuterBox, destination.firstChild);
+    resultSectionOuterBox.className = 'output l-output';
   };
 
   // create three Fizzbuzzes just for sport
@@ -96,52 +96,52 @@ var fizzBuzzFrontEnd = (function() {
   };
 
   // activate first FizzBuzz input area
-  document.getElementById("submit").addEventListener("click", function(e) {
+  document.getElementById('submit').addEventListener('click', function(e) {
     e.preventDefault();
     var firstUserInputNumber = parseInt(document.getElementById('number-one').value) || 0;
     var secondUserInputNumber = parseInt(document.getElementById('number-two').value) || 0;
     fbFizzBuzz.input(firstUserInputNumber, secondUserInputNumber);
-    document.getElementById('number-one').value = "";
-    document.getElementById('number-two').value = "";
-    write(fbFizzBuzz, document.getElementById("result-box"), firstUserInputNumber, secondUserInputNumber);
-    // fbFizzBuzz.write(document.getElementById("result-box"));
+    document.getElementById('number-one').value = '';
+    document.getElementById('number-two').value = '';
+    write(fbFizzBuzz, document.getElementById('result-box'), firstUserInputNumber, secondUserInputNumber);
+    // fbFizzBuzz.write(document.getElementById('result-box'));
   });
 
-  document.getElementById("clear").addEventListener("click", function(e) {
+  document.getElementById('clear').addEventListener('click', function(e) {
     e.preventDefault();
-    emptyBox(document.getElementById("result-box"));
+    emptyBox(document.getElementById('result-box'));
   });
 
   // activate second FizzBuzz input area
-  document.getElementById("submitb").addEventListener("click", function(e) {
+  document.getElementById('submitb').addEventListener('click', function(e) {
     e.preventDefault();
     var firstUserInputNumber = parseInt(document.getElementById('number-oneb').value) || 0;
     var secondUserInputNumber = parseInt(document.getElementById('number-twob').value) || 0;
     fbBleepBlorp.input(firstUserInputNumber, secondUserInputNumber);
-    document.getElementById('number-oneb').value = "";
-    document.getElementById('number-twob').value = "";
-    write(fbBleepBlorp, document.getElementById("result-boxb"), firstUserInputNumber, secondUserInputNumber);
+    document.getElementById('number-oneb').value = '';
+    document.getElementById('number-twob').value = '';
+    write(fbBleepBlorp, document.getElementById('result-boxb'), firstUserInputNumber, secondUserInputNumber);
   });
 
-  document.getElementById("clearb").addEventListener("click", function(e) {
+  document.getElementById('clearb').addEventListener('click', function(e) {
     e.preventDefault();
-    emptyBox(document.getElementById("result-boxb"));
+    emptyBox(document.getElementById('result-boxb'));
   });
 
 // activate third FizzBuzz input area
-  document.getElementById("submitc").addEventListener("click", function(e) {
+  document.getElementById('submitc').addEventListener('click', function(e) {
     e.preventDefault();
     var firstUserInputNumber = parseInt(document.getElementById('number-onec').value) || 0;
     var secondUserInputNumber = parseInt(document.getElementById('number-twoc').value) || 0;
     rollYourOwn.input(firstUserInputNumber, secondUserInputNumber);
-    document.getElementById('number-onec').value = "";
-    document.getElementById('number-twoc').value = "";
+    document.getElementById('number-onec').value = '';
+    document.getElementById('number-twoc').value = '';
     write(rollYourOwn, document.getElementById("result-boxc"), firstUserInputNumber, secondUserInputNumber);
     document.getElementById('first-word').className = '';
     document.getElementById('second-word').className ='';
   });
 
-  document.getElementById("clearc").addEventListener("click", function(e) {
+  document.getElementById('clearc').addEventListener('click', function(e) {
     e.preventDefault();
     document.getElementById('first-word').className = '';
     document.getElementById('second-word').className ='';
@@ -150,7 +150,7 @@ var fizzBuzzFrontEnd = (function() {
   // activate roll your own buttons
   document.getElementById('roll-new').addEventListener('click', function(e) {
     e.preventDefault();
-    rollYourOwn = new FizzBuzz(document.getElementById('first-word').value || 'pen', document.getElementById('second-word').value || 'island');
+    rollYourOwn = new FizzBuzz(document.getElementById('first-word').value || 'stink', document.getElementById('second-word').value || 'foot');
     document.getElementById('first-word').classList.add('clicked');
     document.getElementById('second-word').classList.add('clicked');
   })
